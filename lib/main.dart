e763aa50486d78f5d7022fc2f67f95c7a17f49f8
hend <hendrigeng13@gmail.com>
2023-10-21 01:16:31 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,44 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoappround2/view/task_view.dart';
 import 'package:todoappround2/view_model/view_model.dart';
+fimport 'package:http/http.dart' as http;
+
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
 
 void main() {
   runApp(
